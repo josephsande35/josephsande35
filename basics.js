0595f567d1b268e605ad78638c8390e5a8f2fdfa
josephsande35 <josephsande35@gmail.com>
2025-02-05 15:07:07 +0300

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>HOME PAGE</title>
+    </head>
+    <body>
+        <h1>My Home Page</h1>
+        <p>This is my Home page</p>
+    </body>
+
+    <a href="contactUs.html"><button>Contact us</button>  </a>
+    <br> <br>
+    <a href="aboutUs.html"><button>About us</button> </a>
+
+     <ul type="square">
+        <li>BSIT</li>
+        <li>BSCS</li>
+        <li>BSDS</li>
+        <li>DIT</li>
+     </ul>
+
+     <ol type="i" >
+        <li>BSIT</li>
+        <li>BSCS</li>
+        <li>BSDS</li>
+        <li>DIT</li>
+     </ol>
+    
+
+</html>
